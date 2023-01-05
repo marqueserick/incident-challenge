7dee12427c81eea6a02775c3c35bb3184d87a32b
marqueserick <erickmarques43@gmail.com>
2023-01-05 14:37:42 -0300

    add @Getter annotation

--- src/main/java/com/marqueserick/incidentApi/controller/dto/IncidentPartialDto.java
+++ src/main/java/com/marqueserick/incidentApi/controller/dto/IncidentPartialDto.java
@@ -1,15 +1,11 @@
 package com.marqueserick.incidentApi.controller.dto;
 
+import lombok.Getter;
+
+@Getter
 public class IncidentPartialDto {
 	
 	private String name;
 	private String description;
-	
-	public String getName() {
-		return name;
-	}
-	public String getDescription() {
-		return description;
-	}
 	
 }
