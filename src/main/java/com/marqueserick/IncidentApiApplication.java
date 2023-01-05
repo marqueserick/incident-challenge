a81b03e8a5b2552da325fd9266604145bd57a0d1
marqueserick <erickmarques43@gmail.com>
2023-01-04 21:14:11 -0300

    create project

---
+++ src/main/java/com/marqueserick/IncidentApiApplication.java
@@ -0,0 +1,13 @@
+package com.marqueserick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncidentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marqueserick/IncidentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marqueserick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
