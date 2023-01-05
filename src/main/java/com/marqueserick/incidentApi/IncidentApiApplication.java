6db0752368a9cdd7954f1c3b0ce78ea6670b1195
marqueserick <erickmarques43@gmail.com>
2023-01-04 21:46:39 -0300

    update database configuration

--- src/main/java/com/marqueserick/incidentApi/IncidentApiApplication.java
+++ src/main/java/com/marqueserick/incidentApi/IncidentApiApplication.java
@@ -1,4 +1,4 @@
-package com.marqueserick;
+package com.marqueserick.incidentApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
