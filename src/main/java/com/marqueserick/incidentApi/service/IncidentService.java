5fce4fc2712e85435fd27dd44800625fe56e003f
marqueserick <erickmarques43@gmail.com>
2023-01-06 15:00:12 -0300

    delete unused import

--- src/main/java/com/marqueserick/incidentApi/service/IncidentService.java
+++ src/main/java/com/marqueserick/incidentApi/service/IncidentService.java
@@ -4,8 +4,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import javax.persistence.EntityNotFoundException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
