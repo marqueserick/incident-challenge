e7770725854d683256c7d136a71071192de6a0b8
marqueserick <erickmarques43@gmail.com>
2023-01-06 17:35:10 -0300

    add user to database

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,4 @@
 insert into incident (name, description, created_at) values ('Name Test #1', 'Description for name test #1','2023-01-03 11:43:47');
 insert into incident (name, description, created_at) values ('Name Test #2', 'Description for name test #2','2023-01-04 15:22:13');
+
+insert into user_api (username, password) values ('admin','$2a$12$6DOxiW2bNhuBxu27XKiVHOtlQRRSblFUqL1uepagrsrNu/A0TOU42');
