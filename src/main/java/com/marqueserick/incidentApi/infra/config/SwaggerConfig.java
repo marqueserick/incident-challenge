67f511bd03b02ca06d0a69f17d8df3436c9cb1f0
marqueserick <erickmarques43@gmail.com>
2023-01-05 22:11:14 -0300

    add api info

--- src/main/java/com/marqueserick/incidentApi/infra/config/SwaggerConfig.java
+++ src/main/java/com/marqueserick/incidentApi/infra/config/SwaggerConfig.java
@@ -3,8 +3,11 @@ package com.marqueserick.incidentApi.infra.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,7 +22,17 @@ public class SwaggerConfig {
 		          .select()
 		          .apis(RequestHandlerSelectors.basePackage("com.marqueserick.incidentApi"))
 		          .paths(PathSelectors.ant("/**"))
-		          .build();
+		          .build()
+		          .apiInfo(getApiInfo());
+	}
+	
+	public ApiInfo getApiInfo() {
+		return new ApiInfoBuilder()
+				.title("IncidentAPI")
+				.description("API built as solution for Java Developer Challenge")
+				.version("1.0.0")
+				.contact(new Contact("Erick Marques","https://linkedin.com/in/marqueserick","erickmarques43@gmail.com"))
+				.build();
 	}
 
 }
