adef2b0b8c91039efe827ab7f7fa5f3c5b2df34d
marqueserick <erickmarques43@gmail.com>
2023-01-06 19:01:40 -0300

    add serial version id

--- src/main/java/com/marqueserick/incidentApi/model/User.java
+++ src/main/java/com/marqueserick/incidentApi/model/User.java
@@ -24,6 +24,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class User implements UserDetails{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
